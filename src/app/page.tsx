22e54645bdebac7bea800d04aa2c2fe4fb8dcf49
alison tahiri <alison.tahiri@gmail.com>
2023-06-19 21:48:09 +0200

    feature: created posts list items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,38 @@
 import { getPosts } from "@/lib/services/postServices";
+import Image from "next/image";
+import Link from "next/link";
 
 export default async function Home() {
-  const postsData: Promise<Post[]> = getPosts();
-  const posts = await postsData;
-
-  console.log(postsData);
+  const postsRes: Promise<PostsData> = getPosts();
+  const postsData = await postsRes;
 
   return (
-    <main>
-      <h1 className="text-xl p-4">Welcome to Home Page</h1>
-      <section>{JSON.stringify(posts)}</section>
-    </main>
+    <>
+      <main>
+        <h1 className="text-xl p-4">Posts</h1>
+      </main>
+      <section className="flex flex-col items-center gap-4">
+        {postsData.data.map((p) => (
+          <Link
+            href={`/posts/${p.attributes.slug}`}
+            key={p.id}
+            className="w-full max-w-2xl"
+          >
+            <div className="flex gap-4 items-center bg-slate-600 w-full  rounded-md overflow-clip h-48">
+              <div className="relative h-48 w-96">
+                <Image
+                  src={p.attributes.cover.data.attributes.url || ""}
+                  alt={p.attributes.title}
+                  style={{ objectFit: "cover" }}
+                  fill={true}
+                />
+              </div>
+
+              <h1>{p.attributes.title}</h1>
+            </div>
+          </Link>
+        ))}
+      </section>
+    </>
   );
 }
