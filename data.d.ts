e5fa81080b9ace8c3adac1b67758e1859327a467
alison tahiri <alison.tahiri@gmail.com>
2023-06-19 21:47:36 +0200

    fix: added image types to posts

--- data.d.ts
+++ data.d.ts
@@ -1,21 +1,83 @@
-type Post = {
-  data: {
-    id: number;
-    attributes: {
-      content: string;
-      title: string;
-      slug: string;
-      createdAt: string;
-      updatedAt: string;
-      publishedAt: string;
-    };
-  }[];
+type PostsData = {
+  data: Post[];
   meta: {
     pagination: {
       page: number;
       pageSize: number;
       pageCount: number;
       total: number;
+    };
+  };
+};
+
+type Post = {
+  id: number;
+  attributes: {
+    content: string;
+    title: string;
+    slug: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+    cover: {
+      data: {
+        id: number;
+        attributes: {
+          name: string;
+          alternativeText: string | null;
+          caption: string | null;
+          width: number;
+          height: number;
+          formats: {
+            small: {
+              ext: string;
+              url: string;
+              hash: string;
+              mime: string;
+              name: string;
+              path: string | null;
+              size: number;
+              width: number;
+              height: number;
+              provider_metadata: {
+                public_id: string;
+                resource_type: string;
+              };
+            };
+            thumbnail: {
+              ext: string;
+              url: string;
+              hash: string;
+              mime: string;
+              name: string;
+              path: string | null;
+              size: number;
+              width: number;
+              height: number;
+              provider_metadata: {
+                public_id: string;
+                resource_type: string;
+              };
+            };
+          };
+          hash: string;
+          ext: string;
+          mime: string;
+          size: number;
+          url: string;
+          previewUrl: string | null;
+          provider: string;
+          provider_metadata: {
+            public_id: string;
+            resource_type: string;
+          };
+          createdAt: string;
+          updatedAt: string;
+        };
+      };
+    };
+    categories: {
+      data: [];
     };
   };
 };
